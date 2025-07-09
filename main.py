f084db64d77388c121732c2fd3114335ead93f54
Elvis Sestomi <sestomielvis@gmail.com>
2025-07-09 13:40:03 +0700

    screen closeable

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0))
         pygame.display.flip()
 
 
 if __name__ == "__main__":
     main()
+    
